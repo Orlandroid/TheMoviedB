f63cf689e63123bfa4fd5089e7dd062a4c882f0e
Orlandroid <lopez.orlando.gold9@gmail.com>
2025-09-18 19:56:45 -0600

    Migrate to catalog versions

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -43,12 +43,11 @@ android {
 
 dependencies {
     implementation(project(":domain"))
-    implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.13.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.espresso)
     //Room Dependecies
     implementation("androidx.room:room-runtime:2.8.0")
     implementation("androidx.room:room-ktx:2.8.0")
